1274adcdd18b840bc7a815f0ef55e6f03dd9f65a
Daniel Ansari <d0nsari2020@gmail.com>
2023-03-06 03:17:06 +0700

    add tailwind class

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -15,9 +15,14 @@ const TodoForm = ({newTodo, setNewTodo, onTodoAdd}: Props) => {
   };
 
   return (
-    <form onSubmit={handleNewTodoSubmit}>
-      <input type="text" value={newTodo} onChange={handleNewTodoChange}/>
-      <button type="submit">Add</button>
+    <form onSubmit={handleNewTodoSubmit} className="mb-4">
+      <div className="flex mt-4">
+        <input type="text" value={newTodo} onChange={handleNewTodoChange}
+               className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"/>
+        <button type="submit"
+                className="flex-no-shrink p-2 border-2 rounded text-blue-500 border-blue-500 hover:text-white hover:bg-blue-400">Add
+        </button>
+      </div>
     </form>
   );
 };
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { TodoDocument} from '@/models/todo';
+import {TodoDocument} from '@/models/todo';
 
 interface TodoItemProps {
   todo: TodoDocument;
@@ -49,37 +49,47 @@ const TodoItem: React.FC<TodoItemProps> = ({
   };
 
   return (
-    <li>
-      <label>
+    <div className="flex mb-4 items-center">
+      <label className="w-full text-grey-darkest flex items-center">
         <input
+          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
           type="checkbox"
           checked={todo.completed}
           onChange={handleCheckboxChange}
         />
-        <span className={todo.completed ? 'line-through' : ''}>
-    {isEditing ? (
-      <input
-        type="text"
-        value={editedTitle}
-        onChange={handleTitleChange}
-      />
-    ) : (
-      todo.title
-    )}
-  </span>
+        <span className={todo.completed ? 'ml-2 line-through' : 'ml-2'}>
+          {isEditing ? (
+            <input
+              type="text"
+              value={editedTitle}
+              onChange={handleTitleChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 mr-4 text-grey-darker"
+            />
+          ) : (
+            todo.title
+          )}
+        </span>
       </label>
       {isEditing ? (
         <>
-          <button onClick={handleSaveEditButtonClick}>Save</button>
-          <button onClick={handleCancelEditButtonClick}>Cancel</button>
+          <button onClick={handleSaveEditButtonClick}
+                  className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green-500 border-green hover:bg-green-500">Save
+          </button>
+          <button onClick={handleCancelEditButtonClick}
+                  className="flex-no-shrink p-2 ml-2 border-2 rounded text-red-500 border-red-500 hover:text-white hover:bg-red-500">Cancel
+          </button>
         </>
       ) : (
         <>
-          <button onClick={handleEditButtonClick}>Edit</button>
-          <button onClick={handleDeleteButtonClick}>Delete</button>
+          <button onClick={handleEditButtonClick}
+                  className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green-500 border-green-500 hover:bg-green-400">Edit
+          </button>
+          <button onClick={handleDeleteButtonClick}
+                  className="flex-no-shrink p-2 ml-2 border-2 rounded text-red-500 border-red-500 hover:text-white hover:bg-red-400">Delete
+          </button>
         </>
       )}
-    </li>
+    </div>
   );
 };
 
